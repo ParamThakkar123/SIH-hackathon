df8c623889ba5ffc0f5999e99aeb9e078a2c72d2
umangduwani <umangduwani@gmail.com>
2023-09-07 23:10:47 +0530

    voice page frontend added

--- src/components/Models Page/Voice.jsx
+++ src/components/Models Page/Voice.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Voice = () => {
   return (
-    <div>Voice</div>
+    <div>
+      <h1 className='text-center text-4xl mt-10 mb-10 capitalize'>mental health recognition using voice in real-time</h1>
+      <p className='text-center text-2xl'>Here we used a deep learning model which is trained on a large dataset. This model predicts the mental state of a person using his/her voice which is recorded in real-time</p>
+
+      <button className='text-center m-auto flex justify-center'>Start Recording</button>
+    </div>
   )
 }
 
