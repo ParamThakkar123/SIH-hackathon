ca18d6e6d96fc7014f7e1f72a18c2c0c18b568e1
Rajat0989 <rajatchandarana09@gmail.com>
2023-09-09 14:45:26 +0530

    changes done

--- src/components/Models Page/Voice.jsx
+++ src/components/Models Page/Voice.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { WaveForm } from 'wavesurfer-react'
+import { useState, useEffect } from 'react'
+import { WaveSurfer } from 'wavesurfer-react/dist/utils/createWavesurfer'
 
 const Voice = () => {
   return (
@@ -7,8 +10,10 @@ const Voice = () => {
       <p className='text-center text-2xl'>Here we used a deep learning model which is trained on a large dataset. This model predicts the mental state of a person using his/her voice which is recorded in real-time</p>
 
       <div className='m-0 absolute inset-y-1/2 ml-20 bg-violet-400 translate-y-2/4'>
-      <button className='bg-violet-300 p-5 rounded-xl text-white font-bold'>Start Recording</button>
+      <button className='bg-violet-500 hover:bg-violet-800 transition-colors duration-200 p-5 rounded-xl text-white font-bold'>Start Recording</button>
       </div>
+
+      <div className='mic'></div>
     </div>
   )
 }
