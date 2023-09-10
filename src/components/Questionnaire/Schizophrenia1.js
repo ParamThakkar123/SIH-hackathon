4f766bf0a944cce92e61a33601dd30b412cf2cef
paramthakkar123 <paramthakkar864@gmail.com>
2023-09-10 22:24:19 +0530

    quiz questions

---
+++ src/components/Questionnaire/Schizophrenia1.js
@@ -0,0 +1,46 @@
+export const quiz1 = {
+    topic: 'Schizophrenia',
+    totalQuestions: 10,
+    questions: [
+      {
+        question : "How often you ever hear or see things that others cannot?",
+        answers : ["Never", "Rarely", "Sometimes", "Often", "Very Often"]
+      },
+      {
+        question : "How often other people have a difficult time guessing your emotions by your facial expressions?",
+        answers: ["Never", "Rarely", "Sometimes", "Often", "Very Often"]
+      },
+      {
+        question : "How often you feel that you are being tracked, followed, or watched at home or outside? ",
+        answers: ["Never", "Rarely", "Sometimes", "Often", "Very Often"]
+      },
+      {
+        question : "Are you struggling with maintaining social relationships and academic demands? ",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Do other people say that it is difficult for you to stay on subject or for them to understand you? ",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Do you find it difficult to organize or keep track of your thinking? ",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Do you feel that you have powers that other people cannot understand or appreciate?",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Do you struggle to keep up with daily living tasks such as showering, changing clothes, cleaning ?",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Do you get the sense that others are controlling your thoughts and emotions? ",
+        answers : ["Yes","No"]
+      },
+      {
+        question : "Are you isolating from others ?",
+        answers : ["Yes","No"]
+      }
+    ],
+  }
