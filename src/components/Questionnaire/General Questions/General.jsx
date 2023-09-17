5fbb69f8a1ac7e1131f4fc7a7ce932f5e06a01ae
umangduwani <umangduwani@gmail.com>
2023-09-18 00:17:48 +0530

    added file for general questions

---
+++ src/components/Questionnaire/General Questions/General.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const General = () => {
+  return (
+    <div>
+        <h1>Take a Test </h1>
+        Answer the following questions so that we can accurately assess your mental health and provide you the best solutions possible
+
+    </div>
+  )
+}
+
+export default General
