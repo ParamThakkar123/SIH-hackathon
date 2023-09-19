bc91b2bba1d1d177ac6ddf83dd3291430b21c58b
Rajat0989 <rajatchandarana09@gmail.com>
2023-09-19 16:34:03 +0530

    Sign UP

--- src/components/Sign Up Page/Studentsign.jsx
+++ src/components/Sign Up Page/Studentsign.jsx
@@ -22,148 +22,132 @@ const Studentsign = () => {
 
   return (
     <>
-      <section className="loginsection register-section ">
-        <div className="container grid grid-two-column">
+      <section className="loginsection register-section h-screen flex justify-center items-center">
+        <div className="bg-gray-200 p-8 rounded-3xl">
           <div className="login-image">
             {/* <img src="/hero.png" alt="user loggin pic" /> */}
             {/* <img src={heroImage} className="hero-image" alt="user login " /> */}
           </div>
           {/* right side div  */}
           <div className="login-data register-data">
-            <p className="bold-text">Create Your Account!</p>
+            <p className="bold-text text-center text-4xl">Create Your Account!</p>
             {/* form start  */}
-            <form action="#">
-              <div className="form">
+            <form className="flex flex-col" action="# ">
+              
                 <input
                   type="text"
                   id="email"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Email"
                   name="username"
                   value={userRegister.username}
                   onChange={handleInput}
                 />
-                <label htmlFor="email" className="form__label">
-                  Email Address
-                </label>
-              </div>
+                
+              
 
               {/* for mobile number  */}
-              <div className="form">
+              
                 <input
                   type="text"
                   id="phone"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Your Contact Number"
                   name="Sphone"
                   value={userRegister.Sphone}
                   onChange={handleInput}
                 />
-                <label htmlFor="phone" className="form__label">
-                  Student Contact No.
-                </label>
-              </div>
+                
+              
 
-              <div className="form">
+              
                 <input
                   type='number'
                   id="pasword"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Age"
                   name="age"
                   value={userRegister.age}
                   onChange={handleInput}
                 />
-                <label htmlFor="age" className="form__label">
-                  Age
-                </label>
-              </div>
+                
+              
 
-              <div className="form">
+              
                 <input
                   type="text"
                   id="institute"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Institute"
                   name="institute"
                   value={userRegister.institute}
                   onChange={handleInput}
                 />
-                <label htmlFor="institute" className="form__label">
-                  Institute
-                </label>
-              </div>
+                
+              
 
-              <div className="form">
+              
                 <input
                   type="text"
                   id="phone"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Parents' Contact Number"
                   name="Pphone"
                   value={userRegister.Pphone}
                   onChange={handleInput}
                 />
-                <label htmlFor="phone" className="form__label">
-                  Parents' Contact No.
-                </label>
-              </div>
+                
+              
 
-              <div className="form">
+              
                 <input
                   type="text"
                   id="phone"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Guardian Contact Number"
                   name="Gphone"
                   value={userRegister.Gphone}
                   onChange={handleInput}
                 />
-                <label htmlFor="phone" className="form__label">
-                  Guardian Contact No.
-                </label>
-              </div>
+                
+              
 
-              <div className="form">
+              
                 <input
                   type="password"
                   id="pasword"
-                  className="form__input"
-                  placeholder=" "
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
+                  placeholder="password"
                   name="password"
                   value={userRegister.password}
                   onChange={handleInput}
                 />
-                <label htmlFor="pasword" className="form__label">
-                  Password
-                </label>
-              </div>
+               
+              
 
-              <div className="form">
+              
                 <input
                   type="text"
                   id="repeat password"
-                  className="form__input"
+                  className="form__input px-4 py-1 m-2 rounded-3xl"
                   placeholder="Confirm Password"
                   name="repeatpassword"
                   value={userRegister.cpassword}
                   onChange={handleInput}
                 />
-                <label htmlFor="repeat password" className="form__label">
-                  Confirm Password
-                </label>
-              </div>
+                
+              
 
-              <div className="checkbox">
-                <input type="checkbox" />
-                <p className="common-para">
+              <div className="pl-4">
+                <input className='inline' type="checkbox" />
+                <p className="pl-4 inline">
                   By clicking the checkbox, I agree to Playwise{" "}
                   <a href="#"> Terms & Conditions </a>
                 </p>
               </div>
-              <div className="checkbox">
-                <input type="checkbox" />
-                <p className="common-para">
+              <div className="pl-4">
+                <input type="checkbox" className='inline'/>
+                <p className="pl-4 inline">
                   Two Factor Authentication (Dual Layer Security)
                 </p>
               </div>
@@ -171,7 +155,7 @@ const Studentsign = () => {
               <input
                 type="submit"
                 value="REGISTER"
-                className="btn register-btn"
+                className="btn register-btn bg-indigo-500 rounded-3xl text-white p-3 m-3"
               />
             </form>
             {/* extra way to login  */}
@@ -189,7 +173,7 @@ const Studentsign = () => {
               <i className="fa-solid fa-m"></i>
               <i className="fa-brands fa-linkedin-in"></i>
             </div>
-            <p className="common-para">
+            <p className="common-para text-center m-3">
               Already have an account with us? <a href="/login"> Login </a> here
             </p>
           </div>
